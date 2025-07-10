513fb92ecf6ff95b09c32ffdae60ddfc34a833a0
WhaleFall <166845494@qq.com>
2025-07-10 17:31:34 +0800

    test

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script deploys the application to a remote server using SSH and rsync.
+# Usage: ./deploy.sh "commit message"
+
+export msg="$1"
+export flag="-m"
+
+if [ -z "$msg" ]; then
+    export flag="--allow-empty"
+fi
+
+git add .
+git commit $flag "$msg"
+git push origin main
